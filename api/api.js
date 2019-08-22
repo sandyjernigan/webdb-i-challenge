f2bb05c08d4521cffa78071a217d91d1d0fece4e
Sandy <sandy.jernigan@gmail.com>
2019-08-21 20:04:15 -0400

    SELECT all from accounts

--- api/api.js
+++ api/api.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require('../data/dbConfig.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // SELECT * FROM Posts;
-  db('accounts');
+router.get('/', async (req, res) => {
+  try {
+    // SELECT * FROM accounts;
+    const results = await db('accounts');
+    res.status(200).json(results);
+  } catch (error) {
+    console.log(error);
+    next({ code: 500, message: "The information could not be retrieved." });
+  }
 });
 
 router.get('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
-
+// Express
 const server = express();
-
 server.use(express.json());
+
+// Routers
+const ApiRouter = require('./api/api.js');
+server.use('/api/accounts', ApiRouter);
+
+// Base Folder
+server.get('/', (req, res) => {
+  res.send(`<h2>Hello World!</h2>`)
+});
+
+server.use((error, req, res, next) => {
+  let errorMessage = "Error occured: ";
+
+  switch (error.code) {
+    case 400: errorMessage = errorMessage + "You made a Bad Request. " + error.message;
+      break;
+    case 404: errorMessage = errorMessage + "You made a Bad Request. " + error.message;
+      break;
+    default: errorMessage = errorMessage + "Oh my! " + error.message;
+  }
+  res.status(error.code).json({ 
+    message: errorMessage
+  });
+});
 
 module.exports = server;
